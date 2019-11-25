ea49efaa50b8621715a7da1ae8f143f36e50fc19
KHARITONOV172134 <kharitonov.av@mmk.ru>
2019-11-25 17:23:36 +0500

    доступ к неким страницам только после авторизации на сайте

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { AlkousersComponent } from './alkousers/alkousers.component';
 import { RandomimgComponent } from './randomimg/randomimg.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, CanActivate } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { WorkComponent } from './work/work.component';
 import { BlogComponent } from './blog/blog.component';
@@ -9,13 +9,14 @@ import { ContactsComponent } from './contacts/contacts.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AuthComponent } from './auth/auth.component';
 import { MainComponent } from './main/main.component';
+import { GeneralGuard } from './general.guards';
 
 
 const routes: Routes = [
   { path: '', component: MainComponent  },
   { path: 'About', component: AboutComponent},
-  { path: 'Work', component: WorkComponent},
-  { path: 'Blog', component: BlogComponent},
+  { path: 'Work', component: WorkComponent, canActivate: [ GeneralGuard ]},
+  { path: 'Blog', component: BlogComponent, canActivate: [ GeneralGuard ]},
   { path: 'Contacts', component: ContactsComponent},
   { path: 'Auth', component: AuthComponent},
   { path: 'alkouser', component: AlkousersComponent},
@@ -24,6 +25,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  providers: [GeneralGuard],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
