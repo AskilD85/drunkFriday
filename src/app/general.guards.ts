865a798e21d3e9bd2f3535f9bc05b36d1d5bd5b2
KHARITONOV172134 <kharitonov.av@mmk.ru>
2019-11-25 17:34:10 +0500

    доступ к неким страницам только после авторизации на сайте

---
+++ src/app/general.guards.ts
@@ -0,0 +1,23 @@
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpService } from './services/http.service';
+
+export class GeneralGuard implements CanActivate {
+
+    constructor(private http: HttpService, private route: Router ) {
+    }
+    auth = ''   ;
+    backUrl;
+    canActivate(route2: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+
+        this.auth = localStorage.getItem('apikey');
+
+        if (this.auth) {
+            return true;
+        } else {
+            this.route.navigate(['/Auth']);
+            localStorage.setItem('backUrl', state.url);
+
+        }
+    }
+}
