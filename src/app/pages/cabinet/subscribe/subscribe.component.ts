b731353404a8015c37de5e42f7c9fe11723f9ca4
askild85 <askildar@yandex.ru>
2020-03-09 17:50:26 +0500

    добавление реактивной формы для подписок

--- src/app/pages/cabinet/subscribe/subscribe.component.ts
+++ src/app/pages/cabinet/subscribe/subscribe.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { HttpService } from './../../../services/http.service';
+import { Categories } from 'src/app/model/Categories';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-subscribe',
   templateUrl: './subscribe.component.html',
   styleUrls: ['./subscribe.component.css']
 })
-export class SubscribeComponent implements OnInit {
+export class SubscribeComponent implements OnInit, OnDestroy {
+  categories: Categories;
+  subscribeForm = new FormGroup({
+    title: new FormControl('', Validators.required),
+    city: new FormControl('', Validators.required),
+    subscribe: new FormArray([
 
-  constructor() { }
+    ])
+  });
+  getCategSub: Subscription;
+
+  constructor(private httpService: HttpService) { }
+  get formData() { return this.subscribeForm.get('subscribe') as FormArray; }
 
   ngOnInit() {
+    this.getCategSub = this.httpService.getCategories()
+      .subscribe((categ: Categories) => {
+        this.categories = categ;
+      });
   }
-
+  ngOnDestroy() {
+    if (this.getCategSub) {
+      this.getCategSub.unsubscribe();
+    }
+  }
+  add() {
+    (this.subscribeForm.get('subscribe') as FormArray).push(
+       new FormControl('33', Validators.required) );
+  }
+  destroy(i: number) {
+    this.formData.removeAt(i);
+  }
+  saveForm() {
+    console.log(this.subscribeForm.value);
+  }
 }
