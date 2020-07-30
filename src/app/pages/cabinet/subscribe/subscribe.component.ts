3e667080bf7e68ea5bc9513645f3e7d8c1dc3149
askild85 <askildar@yandex.ru>
2020-07-30 16:53:44 +0500

    Подписка

--- src/app/pages/cabinet/subscribe/subscribe.component.ts
+++ src/app/pages/cabinet/subscribe/subscribe.component.ts
@@ -7,18 +7,19 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-subscribe',
   templateUrl: './subscribe.component.html',
-  styleUrls: ['./subscribe.component.css']
+  styleUrls: ['./subscribe.component.scss']
 })
 export class SubscribeComponent implements OnInit, OnDestroy {
   categories: Categories;
+
   subscribeForm = new FormGroup({
-    title: new FormControl('', Validators.required),
-    city: new FormControl('', Validators.required),
     subscribe: new FormArray([
 
     ])
   });
   getCategSub: Subscription;
+  success = false;
+  message: string;
 
   constructor(private httpService: HttpService) { }
   get formData() { return this.subscribeForm.get('subscribe') as FormArray; }
@@ -29,19 +30,53 @@ export class SubscribeComponent implements OnInit, OnDestroy {
         this.categories = categ;
       });
   }
+
   ngOnDestroy() {
     if (this.getCategSub) {
       this.getCategSub.unsubscribe();
     }
   }
+
+  getSubscribes() {
+    null;
+  }
+
+  
   add() {
     (this.subscribeForm.get('subscribe') as FormArray).push(
-       new FormControl('33', Validators.required) );
+      new FormGroup({
+        city_id: new FormControl('', [Validators.required]),
+        category_id: new FormControl('', [Validators.required]),
+        type: new FormControl('', [Validators.required]),
+        author_id: new FormControl(localStorage.getItem('user_id'), [Validators.required])
+      }));
   }
+
+
   destroy(i: number) {
     this.formData.removeAt(i);
   }
+
   saveForm() {
     console.log(this.subscribeForm.value);
+    if (this.subscribeForm.invalid) {
+      console.log('ивыавлид');
+    } else {
+      this.httpService.subscribe(this.subscribeForm.value.subscribe).subscribe(
+        (data: any) => {
+          console.log(data);
+          if (data.res === 'OK') {
+            this.success = true;
+            this.message = data.msg;
+          }
+
+        },
+      );
+    }
+
+
+
   }
+
+
 }
