87aa3a2d7f4606b9dd10e8aba3daf74e83f3201a
askild85 <askildar@yandex.ru>
2020-08-07 16:05:22 +0500

    Правка удаления подписки без сохранения

--- src/app/pages/cabinet/subscribe/subscribe.component.ts
+++ src/app/pages/cabinet/subscribe/subscribe.component.ts
@@ -5,9 +5,10 @@ import { Categories } from 'src/app/model/Categories';
 import { Subscription } from 'rxjs';
 import { SubscribeService } from './../../../services/subscribe.service';
 import { Subscribe } from 'src/app/model/Subscribe';
-import { MatProgressSpinnerModule } from '@angular/material';
+import { MatProgressSpinnerModule, MatFormField } from '@angular/material';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/admin/auth.service';
+import { SharedService } from 'src/app/services/shared.service';
 @Component({
   selector: 'app-subscribe',
   templateUrl: './subscribe.component.html',
@@ -35,7 +36,8 @@ export class SubscribeComponent implements OnInit, OnDestroy {
   constructor(private httpService: HttpService,
               private subscribeService: SubscribeService,
               private router: Router,
-              private authService: AuthService) { }
+              private authService: AuthService,
+              private sharedService: SharedService) { }
 
 get formData() { return this.subscribeForm.get('subscribe') as FormArray; }
 
@@ -66,11 +68,12 @@ get formData() { return this.subscribeForm.get('subscribe') as FormArray; }
     const userId = localStorage.getItem('user_id');
     this.subscribeService.getSubscribes(userId).subscribe(
       (data: Subscribe[]) => {
-        console.log('here - ' + data[0]);
+        console.log( data);
         this.subscribes = data;
         // заполнить форму подписками
         if (this.subscribes.length > 0) {
-          this.fillForm(this.subscribes);
+          this.fillForm(this.subscribes, this.subscribeForm);
+          console.log('форма - ', this.subscribeForm.controls.subscribe);
         }
         this.showSpinner = false;
       },
@@ -84,17 +87,18 @@ get formData() { return this.subscribeForm.get('subscribe') as FormArray; }
   }
 
 
-  fillForm(arr: Subscribe[]) {
+  fillForm(arr: Subscribe[], form: FormArray | FormGroup) {
+    this.subscribeService.clearForm(form, 'subscribe');
     // tslint:disable-next-line: prefer-for-of
     for (let i = 0; i < arr.length; i++) {
-      console.log(arr[i].city_id, typeof arr[i].city_id);
       // (this.subscribeForm.get('subscribe') as FormArray).push(
       (this.subscribeForm.controls.subscribe as FormArray).push(
         new FormGroup({
           category_id: new FormControl(arr[i].category_id, [Validators.required]),
           type: new FormControl(arr[i].type, [Validators.required]),
           city_id: new FormControl(arr[i].city_id, [Validators.required]),
-          author_id: new FormControl(localStorage.getItem('user_id'), [Validators.required])
+          author_id: new FormControl(localStorage.getItem('user_id'), [Validators.required]),
+          id: new FormControl(arr[i].id)
        }));
     }
   }
@@ -106,35 +110,39 @@ get formData() { return this.subscribeForm.get('subscribe') as FormArray; }
         city_id: new FormControl('', [Validators.required]),
         category_id: new FormControl('', [Validators.required]),
         type: new FormControl('', [Validators.required]),
-        author_id: new FormControl(localStorage.getItem('user_id'), [Validators.required])
+        author_id: new FormControl(localStorage.getItem('user_id'), [Validators.required]),
       }));
   }
 
 
-  destroy(i: number) {
-    console.log(this.subscribes[i].id);
+  destroy(i: number, id?: number) {
     this.disabled = true;
-    this.subscribeService.destroy(this.subscribes[i].id).subscribe(
+    if (id === null || id === undefined) {
+      this.formData.removeAt(i);
+      this.disabled = false;
+    } else {
+
+    this.subscribeService.destroy(id).subscribe(
       () => {
         this.formData.removeAt(i);
         this.disabled = false;
-       },
+      },
       (err) => { console.log(err); }
     );
-
+    }
   }
 
   saveForm() {
-    console.log(this.subscribeForm.value);
     if (this.subscribeForm.invalid) {
-      console.log('ивыавлид');
     } else {
+      this.showSpinner = true;
       this.subscribeService.saveForm(this.subscribeForm.value.subscribe).subscribe(
         (data: any) => {
-          console.log(data);
           if (data.res === 'OK') {
             this.success = true;
             this.message = data.msg;
+            this.fillForm(data.data, this.subscribeForm);
+            this.showSpinner = false;
           }
 
         },
