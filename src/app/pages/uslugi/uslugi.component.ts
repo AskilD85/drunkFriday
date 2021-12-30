0c94e3b3daf02d0e646a08c7e10b770c884e4a14
kharitonov172134 <kharitonov.av@mmk.ru>
2021-12-30 21:49:44 +0500

    рефакторинг

--- src/app/pages/uslugi/uslugi.component.ts
+++ src/app/pages/uslugi/uslugi.component.ts
@@ -20,11 +20,7 @@ export class UslugiComponent implements OnInit, OnDestroy {
   categories: Categories[] = [];
   category: Categories[];
   cities: City[];
-<<<<<<< Updated upstream
-  position = localStorage.getItem('position') !== null ? localStorage.getItem('position') : '1';
-=======
   position = localStorage.getItem('position') !== null ? localStorage.getItem('position') : 'master';
->>>>>>> Stashed changes
   location = '1';
   sArticles: Subscription;
   showSpinner = false;
@@ -46,11 +42,7 @@ export class UslugiComponent implements OnInit, OnDestroy {
   }
 
   dataload() {
-<<<<<<< Updated upstream
-     this.http.getTypes().subscribe(
-=======
      this.http.getPostTypes().subscribe(
->>>>>>> Stashed changes
        (data: ArticleType[] ) => {
          console.log(data);
          this.articleType = data;
